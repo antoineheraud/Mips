2da44a500ce9dd63f4a430c6dc234255c2b3a251
antoineheraud <45139344+antoineheraud@users.noreply.github.com>
2018-11-20 17:02:52 +0100

    Add files via upload

--- step2/src/lecture_instruction.c
+++ step2/src/lecture_instruction.c
@@ -35,19 +35,20 @@ IT nouvinst(char* token, char* type, int nbop, int nline, int dec, OPINST opt[])
 }
 
 
-LISTE lecture_instruction(char* token, char* type, int* nbop, int* nline, int* dec, OPINST* opt, LISTE l, inst_def_t* dict, int* p_nb_inst){
+LISTE lecture_instruction(char* token, char* type, int* pnbop, int* nline, int* dec, OPINST* opt, LISTE l, inst_def_t* dict, int* p_nb_inst){
 		LEXEM c = l->val;
-		int nb_opm;int j;
+		int nb_opm;int j;int nbop;
 		printf("lec_it\n");
+		OPINST opi = calloc(1,sizeof(*opi));
 		int i;
 		for(i=0; i< *p_nb_inst; i++){printf("boucle1\n");printf("%s\n",c->obj);printf("%s\n", dict[i].symbole);
 			if(!strcmp(c->obj, dict[i].symbole)){printf("if1\n");
 				strcpy(token, c->obj);
 				*nline = c->nline;
 				strcpy(type, "SYMB");
-				/* *nbop = dict[i].nb_op*/;
+				/* nbop = dict[i].nb_op*/;
 				nb_opm = dict[i].nb_op;
-				*nbop = 0;
+				nbop = 0;
 				printf("ici\n");
 				l = l-> suiv;
 				c = l->val;
@@ -55,66 +56,68 @@ LISTE lecture_instruction(char* token, char* type, int* nbop, int* nline, int* d
 			}
 		}
 
-		while (*nbop<nb_opm){printf("boucle\n");printf("%s",c->obj);
-				/*while(c->obj != '\0'){printf("while\n" );*/
+		while (nbop<nb_opm){printf("boucle\n");printf("%s",c->obj);
+			opi = calloc(1,sizeof(*opi));
 
-			if(c->type == REG || c->type == HEX || c->type == DECIM){printf("nbop %d\n", *nbop); printf("w2\n");
-				*nbop = *nbop + 1;printf("nbop %d\n", *nbop);
-					if(c->type = REG){printf("REG\n");
-							strcpy(opt[*nbop-1]->token, c->obj);printf("1\n");
-							opt[*nbop-1]->type = REGI;printf("2\n");
+			nbop= 1+nbop;printf("nbop %d\n", nbop);
+			if(c->type == REG || c->type == HEX || c->type == DECIM || c->type == SYMB){ printf("w2\n");
+
+				if(c->type = REG){printf("REG\n");
+							strcpy(opi->token, c->obj);
+							opi->type = REGI;
 						}
 
 					else if(c->type = HEX){printf("HEX\n");
 						if(!strcmp(dict[i].symbole, "ADDI") || !strcmp(dict[i].symbole, "LUI") || !strcmp(dict[i].symbole, "LI")){
-							strcpy(opt[*nbop-1]->token, c->obj);
-							opt[*nbop-1]->type = IMMEDIATE;
+							strcpy(opi->token, c->obj);
+							opi->type = IMMEDIATE;
 							}
 						else if(!strcmp(dict[i].symbole, "ROTR") || !strcmp(dict[i].symbole, "SLL") || !strcmp(dict[i].symbole, "SRL")){
-							strcpy(opt[*nbop-1]->token, c->obj);
-							opt[*nbop-1]->type = SA;
+							strcpy(opi->token, c->obj);
+							opi->type = SA;
 							}
 						}
 
 					else if(c->type = DECIM){printf("DECIM\n");
 						if(((LEXEM)l->suiv->val)->type == P_GAU){   /* endroit intéréssant*/
-							opt[*nbop-1]->type = BASE_OFFSET;
+							opi->type = BASE_OFFSET;
 							while(c->type != P_DROI){
-								strcat(opt[*nbop-1]->token, c->obj);
+								strcat(opi->token, c->obj);
 								l = l->suiv;
 								c = l->val;
 							}
 						}
 						else if(!strcmp(dict[i].symbole, "ADDI") || !strcmp(dict[i].symbole, "LUI") || !strcmp(dict[i].symbole, "LI")){
-							strcpy(opt[*nbop-1]->token, c->obj);
-							opt[*nbop-1]->type = IMMEDIATE;
+							strcpy(opi->token, c->obj);
+							opi->type = IMMEDIATE;
 							}
 						else if(!strcmp(dict[i].symbole, "ROTR") || !strcmp(dict[i].symbole, "SLL") || !strcmp(dict[i].symbole, "SRL")){
-								strcpy(opt[*nbop-1]->token, c->obj);
-								opt[*nbop-1]->type = SA;
+								strcpy(opi->token, c->obj);
+								opi->type = SA;
 								}
 
 						}
-						l = l->suiv;
-						c = l->val;
-						printf("finw2\n");printf("nbop %d\n", *nbop);
-					}
 
-				if(*nbop > dict[i].nb_op){
+						printf("finw2\n");printf("nbop %d\n", nbop);
+						opt[nbop - 1] = opi;
+					} /* fin if reg/hex/decim */
+
+				if(nbop > dict[i].nb_op){
 					printf("Erreur d'incohérence de nombre d'opérandes à la ligne %d \n", *nline);
 					return NULL;}
 				printf (" hey\n");printf("%s\n",c->obj);
+				l = l->suiv;
+				c = l->val;
 				if (!strcmp(c->obj, ",")){printf ("ouf\n");
 					l = l->suiv;
 					c = l->val;
-					*nbop = *nbop - 1;
+
 				}
 				if (!strcmp(c->obj, "\0")){
 					break;
-				}
-				l = l->suiv;
-				c = l->val;
+				}   /* fin if saut de ligne */
 
-					};
 
+			};   /* fin du grand while */
+			*pnbop = nbop;
 			return l;}
