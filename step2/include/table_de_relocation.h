8aa43b99bbc87e3f9d0ebed3da9e31bf255c3d5b
phong21397 <45147067+phong21397@users.noreply.github.com>
2018-11-26 19:06:45 +0100

    Add files via upload
    Nouvelles fichiers .h pour l'adaptation

--- step2/include/table_de_relocation.h
+++ step2/include/table_de_relocation.h
@@ -23,15 +23,18 @@
 /*enum type{R_MIPS_32=2, R_MIPS_26=4, R_MIPS_HI16=5, R_MIPS_LO16=6};*/
 
 struct table_de_relocation{
-	char nom[STRLEN];					/*nom de l'étiquette déclarée dans la table des symboles*/
+	char nom[STRLEN];					/*nom de la section, soit .text soit .data*/
 	unsigned int adresse;					/*le décalage correspondant*/
-	/*type type_rel;						/*type de relocation selon l'énumération précédente*/
-	void* psymbol;
+	/*type type_rel;					/*type de relocation selon l'énumération précédente*/
+	void* psymbol;						/*pointeur vers l'étiquette ou le symbole dans la table des symboles*/
 	struct table_de_relocation* suiv; 			/*l'élément suivant dans la table de relocation*/
 }
 
 typedef struct table_de_relocation tabrel;
 
+tabrel charger_table_de_relocation_text(TS tabsym);
+tabrel charger_table_de_relocation_data(TS tabsym);
+TS erreur_table_de_relocation(tabrel tabreloc, char eti[STRLEN]){
 
 
 #endif
