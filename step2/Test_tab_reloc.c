9414d105da18033ad7b2fa7a2d2bf14952ccfa21
phong21397 <45147067+phong21397@users.noreply.github.com>
2018-12-11 16:18:13 +0100

    Add files via upload

--- step2/Test_tab_reloc.c
+++ step2/Test_tab_reloc.c
@@ -19,49 +19,38 @@
 
 tabrel charger_table_de_relocation_text(TS tabsym){		
 	tabrel tabreloc;														/*fonction qui gère et classifie les étiquettes dans la table de symboles*/
-	tabrel tabreloc1 = tabreloc;
-	while(tabsym != NULL){
 		if(!strcmp(tabsym->section, "TEXT"){			/*cette fonction crée la table de relocation TEXT*/ 
 			tabreloc->nom = tabsym->section;				/*le nom de la section du tableau de relocation*/ 
 			tabreloc->adresse = tabsym->dec;				/*l'adresse du tableau de relocation = son décalage dans la table de symboles*/
 																							/*il faut que le décalage soit positif, regarder la fonction lecture de 2 collections 
 																							  correspondantes*/
 			tabreloc->psymbol = tabsym;							/*pointeur pointe vers la case correspondant dans la table de symboles*/
-			tabreloc = tabreloc->suiv;
-		}
-			tabsym = tabsym->suiv;
-	}
-		tabreloc->suiv = NULL;
-		return tabreloc1;
+		return tabreloc;}
+    else return NULL;
 }
 
 
 tabrel charger_table_de_relocation_data(TS tabsym){		
 	tabrel tabreloc;														/*fonction qui gère et classifie les étiquettes dans la table de symboles*/
-	tabrel tabreloc1 = tabreloc;
-	while(tabsym != NULL){
 		if(!strcmp(tabsym->section, "DATA"){			/*cette fonction crée la table de relocation DATA*/ 
 			tabreloc->nom = tabsym->section;				/*le nom de la section du tableau de relocation*/ 
 			tabreloc->adresse = tabsym->dec;				/*l'adresse du tableau de relocation = son décalage dans la table de symboles*/
 																							/*il faut que le décalage soit positif, regarder la fonction lecture de 2 collections 
 																							  correspondantes*/
 			tabreloc->psymbol = tabsym;							/*pointeur pointe vers le case correspondant dans la table de symboles*/
-			tabreloc = tabreloc->suiv;
-		}
-			tabsym = tabsym->suiv;
-	}
-		tabreloc->suiv = NULL;
-		return tabreloc1;
+		return tabreloc;}
+    else return NULL;
 }
 	
 		
 		
-TS erreur_table_de_relocation(tabrel tabreloc, char eti[STRLEN]){				/*fonction qui cherche si une étiquette existe ou non*/
-	TS pointeur;																/*pointeur sur la case de l'étiquette indiquée*/
-	while(tabreloc != NULL){	
-		pointeur = tabreloc->psymbol; 
-		if(!strcmp(pointeur->etiq, eti))	return pointeur;				/*retourner l'adresse de cette étiquette dans la table de symboles*/
-		tabreloc = tabreloc->suiv;
+TS erreur_table_de_relocation(LISTE TBR, char eti[STRLEN]){				/*fonction qui cherche si une étiquette existe ou non*/
+	TS pts;																/*pointeur sur la case de l'étiquette indiquée*/
+  if(TBR == NULL) return NULL;					/*si table symbole est vide, pas d'étiquettes*/
+	while(TBR != NULL){
+		pts = TBR->val; 
+		if(!strcmp(pts->etiq, eti))	return pts;				/*retourner l'adresse de cette étiquette dans la table de symboles*/
+		TBR = TBR->suiv;
 	}
 	return NULL;																								/*retourner NULL car l'étiquette n'existe pas*/
 }
